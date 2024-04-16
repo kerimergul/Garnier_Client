850d708dd1c34fbf507c3b48ee66754ae788ee0d
Kerim Ergul <52758478+kerimergul@users.noreply.github.com>
2024-04-16 14:34:49 +0300

    commit
    -- add demo serial key

--- src/framed_screens/1080_1920.js
+++ src/framed_screens/1080_1920.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import '../styles/1080_1920.css';
+import '../styles/serial.css';
 
 const style = { backgroundImage: 'url(/backgrounds/1080_1920.png)' };
 
@@ -111,6 +112,7 @@ class _1080_1920 extends Component {
                 <video id="video" loop className="video_1080_1920" height="1516.8" width="708.48" autoPlay="true" muted="true"></video>
                 <video id="video2" loop className="video_1080_1920" height="1516.8" width="708.48" autoPlay="true" muted="true"></video>
                 <div className="hole"></div>
+                <div id="serial" class="serial">MAT-TR-2400608</div>
             </div>
         );
     }
